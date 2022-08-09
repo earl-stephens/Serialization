1cbba4d0aca2f922e11357eee352a726fe7c8020
earl-stephens <stephensearl@hotmail.com>
2022-08-08 21:08:52 -0600

    Add code for serialization

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,52 @@
 package application;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class Person implements Serializable {
+	/* have to have the interface if going to use
+	 * serialization
+	 */
+	private String name;
+	private int id;
+	
+	public Person(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", id=" + id + "]";
+	}
+	
+	
+	
+}
+
 public class App {
 	public static void main(String[] args) {
+		/* serialization is used for saving and loading
+		 * entire objects
+		 */
 		
+		Person p1 = new Person("Joe", 1);
+		
+		String pathString = "test.bin";
+		
+		try(var os = new ObjectOutputStream(new FileOutputStream(pathString))) {
+			os.writeObject(p1);
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Cannot write file: " + pathString);
+		}
+		catch(IOException e) {
+			System.out.println("Cannot write file: " + pathString);
+		}
+		
+		System.out.println("Completed. " + pathString + " created.");
 	}
 }
